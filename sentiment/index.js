ef78617ee22ec50c2e655ca49a2e9a9edfb530cc
“NotCJSBirdie” <serquinajhunlo@gmail.com>
2025-09-07 21:41:46 -0400

    changes

--- sentiment/index.js
+++ sentiment/index.js
@@ -3,28 +3,25 @@ const express = require('express');
 const axios = require('axios');
 const logger = require('./logger');
 const expressPino = require('express-pino-logger')({ logger });
-
-// Task 1: import the natural library
 const natural = require("natural");
 
-// Task 2: initialize the express server
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(expressPino);
 
-// Task 3: create the POST /sentiment analysis
+// Define the sentiment analysis route
 app.post('/sentiment', async (req, res) => {
-    // Task 4: extract the sentence parameter
     const { sentence } = req.query;
+
 
     if (!sentence) {
         logger.error('No sentence provided');
         return res.status(400).json({ error: 'No sentence provided' });
     }
 
-    // Initialize the sentiment analyzer
+    // Initialize the sentiment analyzer with the Natural's PorterStemmer and "English" language
     const Analyzer = natural.SentimentAnalyzer;
     const stemmer = natural.PorterStemmer;
     const analyzer = new Analyzer("English", stemmer, "afinn");
@@ -32,28 +29,26 @@ app.post('/sentiment', async (req, res) => {
     // Perform sentiment analysis
     try {
         const analysisResult = analyzer.getSentiment(sentence.split(' '));
+
         let sentiment = "neutral";
 
-        // Task 5: set sentiment to negative or positive based on score rules
         if (analysisResult < 0) {
             sentiment = "negative";
-        } else if (analysisResult >= 0 && analysisResult <= 0.33) {
-            sentiment = "neutral";
-        } else {
+        } else if (analysisResult > 0.33) {
             sentiment = "positive";
         }
 
+        // Logging the result
         logger.info(`Sentiment analysis result: ${analysisResult}`);
-
-        // Task 6: success return
+        // Responding with the sentiment analysis result
         res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
-        // Task 7: error return
         res.status(500).json({ message: 'Error performing sentiment analysis' });
     }
 });
 
+// Start the server
 app.listen(port, () => {
     logger.info(`Server running on port ${port}`);
 });
