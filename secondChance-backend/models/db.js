8187e4b8045308ac6409443861c7ff1128297e11
“NotCJSBirdie” <serquinajhunlo@gmail.com>
2025-09-07 21:01:03 -0400

    api endpoint

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -2,27 +2,25 @@
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
-// MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
-
 let dbInstance = null;
 const dbName = `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
     if (dbInstance){
-        return dbInstance
+        return dbInstance;
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+    // Task 2: Connect to MongoDB
+    await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    // Task 3: Connect to correct database and assign to dbInstance
+    dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+    // Task 4: Return the database instance
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
