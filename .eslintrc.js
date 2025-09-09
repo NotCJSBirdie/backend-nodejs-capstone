22a73f646690525ac1eaa05d3bab2f6ee8198d7b
NotCJSBirdie <serquinajhunlo@gmail.com>
2025-09-09 08:39:22 +0800

    changed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,28 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
+    node: true,
   },
-  extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
+  extends: [
+    'standard',
+    // Optional: you could use 'eslint:recommended' for a basic rule set
+    // Or 'plugin:node/recommended' for relaxed node rules
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
-  }
+    // General Style – set to warn or off for flexibility:
+    semi: 'warn', // won’t block build, just warns
+    'comma-dangle': 'off', // allow trailing commas
+    camelcase: 'off', // allow snake_case or PascalCase
+    'space-before-function-paren': 'off', // don’t enforce space
+    'no-multi-spaces': 'warn',
+    'object-shorthand': 'off',
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    'no-unused-vars': ['warn', { args: 'none', ignoreRestSiblings: true }],
+    // You can add/remove more rules depending on your needs
+    // 'indent': ['warn', 2], // only warn if incorrect indentation
+  },
 }
